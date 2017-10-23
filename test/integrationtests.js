02fe4ed0880d6bb640ce28637136d4c566d04816
Andreiskate2 <andrei.ungur@mail.mcgill.ca>
2017-10-22 22:44:31 -0400

    Fix status test
    It was failing because the server wasn't running. It now runs and stops after the test finishes.

--- test/integrationtests.js
+++ test/integrationtests.js
@@ -1,13 +1,21 @@
 var expect  = require('chai').expect;
 var request = require('request');
+var server = require('../server.js');
 
-it('Status', function(done) {
-    request('http://localhost/status' , function(error, response, body) {
-        console.log(error);
-        console.log(response.statusCode);
-        console.log(body);
-        expect(response.statusCode).to.equal(200);
-        expect(body).to.equal('Ok!');
-        done();
+describe('Basic testing', function(){    
+    it('Status', function(done) {
+        request('http://localhost/status' , function(error, response, body) {
+            console.log(error);
+            console.log(response.statusCode);
+            console.log(body);
+            expect(response.statusCode).to.equal(200);
+            expect(body).to.equal('Ok!');
+            done();
+        });
+    });
+
+    //Stop local server instance after tests pass
+    after(function(done) {
+        process.exit();
     });
 });
